e4566def744edf93b147eb62610481aec5135021
BhojGC <gcbhoj@gmail.com>
2026-09-15 19:08:59 -0400

    - login page started

--- zipcart-prod-ionic/src/app/pages/login/login.page.ts
+++ zipcart-prod-ionic/src/app/pages/login/login.page.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import { SignUpCredentials } from '../../DTOs/signUpCredentials';
+import { validateEmail, validatePassword } from '../signup/verificationManager';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  userInput: SignUpCredentials = {
+    emailAddress: '',
+    password: '',
+  };
+  private router = inject(Router);
 
   ngOnInit() {
+    console.log('Hello World From log in');
   }
-
+  // helper methods
+  verifyEmail(): boolean {
+    const email = this.userInput.emailAddress.trim();
+    return validateEmail(email);
+  }
+  verifyPassword(): boolean {
+    const password = this.userInput.password.trim();
+    return validatePassword(password);
+  }
 }
